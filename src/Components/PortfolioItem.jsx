467ff1a56b34f221226122636e779b6edeba2899
Abed001 <abed.alroufe@gmail.com>
2024-03-22 15:56:40 +0200

    making the about page

--- src/Components/PortfolioItem.jsx
+++ src/Components/PortfolioItem.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { useLayoutEffect, useRef } from "react"
+
 
 export default function PortfolioItem({ imageSrc, title, description }) {
 
@@ -44,14 +45,19 @@ export default function PortfolioItem({ imageSrc, title, description }) {
     }, [])
 
 
+
+
     return (
         <div ref={comp} className='relative'>
             <div>
                 <img className='w-[100%] min-h-[100vh] object-cover object-center' src={imageSrc} alt={title} />
-                <h3 className=' absolute bottom-[-100px] text-left leading-7 uppercase text-heading5 tracking-tighter max-w-[400px] lg:text-heading2 lg:right-[-250px] lg:top-[450px] lg:leading-[0.85] lg:max-w-[700px]   '>{title}</h3>
+
+                <h3 className=' absolute bottom-[-100px] text-left leading-7 uppercase text-heading5 tracking-tighter max-w-[400px] lg:text-heading2 lg:right-[-250px] lg:top-[450px] lg:leading-[0.85] lg:max-w-[700px]'>{title}</h3>
+
                 <p id='text2' className='absolute bottom-[-200px] text-left leading-none uppercase text-para tracking-tighter max-w-[200px] lg:left-[-150px] lg:top-[550px] '>{description}</p>
+
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { useLayoutEffect, useRef } from "react"
+import SecondSection from '../Components/SecondSection';
+import mehyo from '/assets/mehyo/mehyo.webp'
 
 export default function About() {
   return (
-    <div>
-      
+    <div className='overflow-x-hidden flex flex-col justify-center items-center px-5 text-white'>
+      <h3 className='textleft leading-8 uppercase text-heading4 tracking-tighter lg:text-heading1 lg:leading-[0.85] lg:w-full mt-40 '>
+        <span className='lg:ml-80 lg:block'>FROM A FERVENT</span> <span className='lg:block'>FIRE—FUELED BY FASHION, MUSIC, AND DESIGN—DAVID
+          <span className='lg:block'>ANTHONY</span> <span className='lg:ml-40'>CHENAULT</span>  <span className='lg:ml-80'>RISES</span></span> </h3>
+
+      <div className='w-[100%] flex flex-col justify-center  lg:flex-row lg:justify-between'>
+        <div className='w-[100%] flex justify-start'>
+          <p id='text2' className='font-bold mt-[-38px] justify-end text-left leading-none uppercase text-smallpara tracking-tighter max-w-[130px] lg:mt-20 lg:text-para lg:max-w-[190px]'>
+            THE MIND THAT SEEKS
+            TO BREAK THE MOLD</p></div>
+
+        <div className='w-[100%] justify-start'>
+          <img className='w-full object-cover object-center lg:w-[1000px] lg:h-[1500px] ' src={mehyo} alt="" />
+        </div>
+      </div>
+
+      <SecondSection />
     </div>
   )
 }
