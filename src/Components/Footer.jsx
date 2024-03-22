5f65a6bd97fc0de52677b98d36b2ea3e3559564a
Abed001 <abed.alroufe@gmail.com>
2024-03-22 23:01:40 +0200

    adding features to about page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link,useLocation } from "react-router-dom";
 import Scramble from 'react-scramble'
 export default function Footer() {
+
+    const location = useLocation();
+
+    const handleClick = () => {
+      if (location.pathname === to) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    };
+
+
     return (
         <div className='font-openSans font-bold text-white text-heading3 lg:text-heading1'>
             <div className=' uppercase hidden-on-phone text-left text-smallpara flex lg:flex-row justify-between items-center w-[100%]'>
                 <p className='font-openSans ml-10'>Mehyo @ 2023</p>
                 <ul className='font-openSans p-0 m-0 list-none lg:flex'>
 
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/"><li className='mr-5 font-openSans'> <Scramble
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/"><li className='mr-5 font-openSans'> <Scramble
                         autoStart={false}
                         mouseEnterTrigger="restart"
                         text="home"
@@ -25,7 +35,7 @@ export default function Footer() {
                         ]}
                     /></li></Link>
 
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/Portfolio"><li className='mr-5 font-openSans'> <Scramble
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/Portfolio"><li className='mr-5 font-openSans'> <Scramble
                         autoStart={false}
                         mouseEnterTrigger="restart"
                         text="portfolio"
@@ -43,7 +53,7 @@ export default function Footer() {
                     /></li></Link>
 
 
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/Contact"><li className='mr-5 font-openSans'> <Scramble
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/Contact"><li className='mr-5 font-openSans'> <Scramble
                         autoStart={false}
                         mouseEnterTrigger="restart"
                         text="contact"
@@ -61,7 +71,7 @@ export default function Footer() {
                     /></li></Link>
 
 
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/About"><li className='mr-5 font-openSans'> <Scramble
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/About"><li className='mr-5 font-openSans'> <Scramble
                         autoStart={false}
                         mouseEnterTrigger="restart"
                         text="about"
@@ -85,10 +95,10 @@ export default function Footer() {
 
             <div className='mt-5 font-openSans lg:hidden md:hidden text-left text-para flex flex-col lg:flex-row items-center w-[100%]'>
                 <ul className='w-[100%] justify-start p-0 ml-40 uppercase list-none lg:flex'>
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/"><li className='mr-5 mb-5 '>Home</li></Link>
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/Portfolio"><li className='mr-5 mb-5'>portfolio</li></Link>
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/Contact"><li className='mr-5 mb-5'>contact</li></Link>
-                    <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/About"><li className='mr-5 mb-5'>about</li></Link>
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/"><li className='mr-5 mb-5 '>Home</li></Link>
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/Portfolio"><li className='mr-5 mb-5'>portfolio</li></Link>
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/Contact"><li className='mr-5 mb-5'>contact</li></Link>
+                    <Link onClick={handleClick} style={{ textDecoration: 'none', color: 'inherit' }} to="/About"><li className='mr-5 mb-5'>about</li></Link>
                     <li className='mb-20'>site by Abed</li>
                 </ul>
 
